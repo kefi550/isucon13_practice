9e739ed54667d5a07fc22c40cb51a01085facfd1
kefi550 <ytiryrstd550@gmail.com>
2024-11-30 17:47:50 +0900

    mysql さいしょ

--- pull.sh
+++ pull.sh
@@ -13,6 +13,8 @@ for i in $(seq 1 3); do
   rsync -avr ${host}:~/env.sh "${LOCAL_PATH}/${host}/"
   rsync -avr ${host}:/etc/hosts "${LOCAL_PATH}/${host}/etc/"
   rsync -avr ${host}:/etc/systemd/system/isupipe-node.service "${LOCAL_PATH}/${host}/etc/systemd/system/"
+  mkdir -p ${LOCAL_PATH}/${host}/etc/mysql/
+  rsync -avr ${host}:/etc/mysql/ "${LOCAL_PATH}/${host}/etc/mysql/"
 done
 
 
